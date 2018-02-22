0d4c2a8c7dab80bc9f02cd58290159a6714bec1a
Taylor Donlan <taylordonlan@gmail.com>
2018-02-22 17:42:26 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -13,13 +13,12 @@ end
 
 
 def countdown_with_sleep(number)
-    if number==0
-      puts "HAPPY NEW YEAR!"
-    else
+
         while number > 0
           puts "#{number} SECOND(S)!"
           number-=1
           sleep(1)
         end
-    end
+        puts "HAPPY NEW YEAR!"
+
   end
