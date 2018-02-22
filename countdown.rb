ba6cbfcc77752a2e0105ef5c81b83b86af3f3022
Taylor Donlan <taylordonlan@gmail.com>
2018-02-22 17:43:00 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,11 @@
 #write your code here
 
 def countdown(number)
-  if number==0
-    puts "HAPPY NEW YEAR!"
-  else
-      while number > 0
-        puts "#{number} SECOND(S)!"
-        number-=1
-      end
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number-=1
   end
+  puts "HAPPY NEW YEAR!"
 end
 
 
